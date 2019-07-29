796867ce199b640a813d7befe3590921ae5567ef
Peter Denno <podenno@gmail.com>
2019-07-29 19:48:49 -0400

    provides filename too

--- mznb/__init__.py
+++ mznb/__init__.py
@@ -8,6 +8,7 @@ import json
 from ipykernel import get_connection_file
 
 import urllib.request
+from notebook import notebookapp
 
 def load_ipython_extension(ipython):
     """
@@ -18,20 +19,22 @@ def load_ipython_extension(ipython):
     # You can register the class itself without instantiating it.  IPython will
     # call the default constructor on it.
 
-    # https://stackoverflow.com/questions/12544056/how-do-i-get-the-current-ipython-notebook-name
     # https://gist.github.com/mbdevpl/f97205b73610dd30254652e7817f99cb
     connection_file_path = get_connection_file()
     connection_file = os.path.basename(connection_file_path)
     kernel_id = connection_file.split('-', 1)[1].split('.')[0]
 
-    #response = urllib.request.urlopen('http://127.0.0.1:8888/api/sessions')
-    #sessions = json.loads(response.read().decode())
-    #filename = 'unknown'
-    #for sess in sessions:
-    #    if sess['kernel']['id'] == kernel_id:
-    #        filename = (sess['notebook']['path'])
-    #        break
-    #server_info = notebook.notebookapp.list_running_servers()
+    filename = 'unknown'
+    last_active = 'unknown'
+    servers = list(notebookapp.list_running_servers())
+    for svr in servers:
+        response = urllib.request.urlopen('http://127.0.0.1:8888/api/sessions?token=' + svr['token'])
+        sessions = json.loads(response.read().decode())
+        for sess in sessions:
+            if sess['kernel']['id'] == kernel_id:
+                ipynb_filename = (sess['notebook']['path'])
+                last_active = (sess['kernel']['last_activity'])
+                break
     
     config_file = str(Path.home()) + "/.local/share/nb-agent/runtime.json"
     with open(config_file) as json_file:
@@ -45,8 +48,8 @@ def load_ipython_extension(ipython):
         short_name = ipython.user_ns['notebook_id']
         msg = {'action' : 'notify',
                'dir' : os.getcwd(),
-               #'ipynb-file' : filename,
-               #'server-info' : server_info,
+               'ipynb-file' : ipynb_filename,
+               'last-active' : last_active, 
                'connection-file' : connection_file_path,
                'short-name' : short_name}
         msg_str = json.dumps(msg)
