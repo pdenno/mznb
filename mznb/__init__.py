3ab830727942971007e7c80d3e8741ab5c2a16d1
Peter Denno <podenno@gmail.com>
2019-09-14 20:46:34 -0400

    integrate nba_gateway

--- mznb/__init__.py
+++ mznb/__init__.py
@@ -6,6 +6,7 @@ from pathlib import Path
 import os
 import zmq
 from ipykernel import get_connection_file
+import nba_gateway
 
 import urllib.request
 from notebook import notebookapp
@@ -17,8 +18,8 @@ def load_ipython_extension(ipython):
     can be loaded via `%load_ext module.path` or be configured to be
     autoloaded by IPython at startup time.
     """
-    # You can register the class itself without instantiating it.  IPython will
-    # call the default constructor on it.
+    # You can register the class itself without instantiating it.  
+    # IPython will call the default constructor on it.
 
     # https://gist.github.com/mbdevpl/f97205b73610dd30254652e7817f99cb
     connection_file_path = get_connection_file()
@@ -46,26 +47,30 @@ def load_ipython_extension(ipython):
         data = json.load(json_file)
         port = data['magic-server-port']
 
+    gw = nba_gateway.NBAgateway()
+    gw.start_server()
+    ipython.push({'nba_gateway': gw})
+
     ctx = zmq.Context()
     sock = ctx.socket(zmq.REQ)
     sock.connect('tcp://localhost:' + str(port))
     try:
         msg = {'action': 'notify',
+               'session-id': session_id,
+               'nba-gateway-port': gw.port,
                'dir': os.getcwd(),
                'ipynb-file': ipynb_filename,
-               'last-active': last_active,
-               'session-id': session_id}
+               'last-active': last_active}
         msg_str = json.dumps(msg)
         sock.send_string(msg_str)
-        print('MiniZinc Notebook Agent Communicator version', __version__,
-              'connected to', session_id, 'at port', str(port))
-#        sock.close()
-#        ctx.term()  # https://github.com/zeromq/pyzmq/issues/831
+        print('MiniZinc Notebook Agent Communicator version %s' % (__version__,))
+        print('Connected to session %s at port %s.' % (session_id, port))
+        print(json.loads(sock.recv()))
+
     except KeyError:
         print('''Not able to communicate with nb-agent.''')
 
     magics = MznbMagics(ipython, port, session_id)
     ipython.register_magics(magics)
-
 
 __version__ = '0.2.dev'
--- mznb/mznb.py
+++ mznb/mznb.py
@@ -19,52 +19,21 @@ class MznbMagics(Magics):
         super(MznbMagics, self).__init__(shell)
         self.port = port
         self.endpoint = 'tcp://localhost:' + str(self.port)
+        self.context = zmq.Context()
         self.session_id = session_id
         self.trans_id = None
         self.sock = None # We thread this thru the different magic functions.
 
     @line_cell_magic
-    def preprocess_mzn(self, line, cell=None):
-        self.trans_id = str(uuid.uuid1())
-        ctx = zmq.Context()
-        self.sock = ctx.socket(zmq.REQ)
-        self.sock.connect(self.endpoint)
-        request = {'action': 'pre-process', 
-                   'session-id': self.session_id,
-                   'trans-id': self.trans_id, 
-                   'nb-dir': os.getcwd(),
-                   'cmd-line': '--print-vars', # POD fix me.
-                   'body': cell}
-        self.sock.send_string(json.dumps(request))
-
-    @line_magic
     def run_mzn(self, line, cell=None, body=None):
-        print(json.loads(self.sock.recv()))
-        self.sock.disconnect(self.endpoint)
-        
-        ctx = zmq.Context()
-        self.sock = ctx.socket(zmq.REQ)
+        self.sock = self.context.socket(zmq.REQ)
         self.sock.connect(self.endpoint)
         request = {'action': 'execute', 
                    'session-id': self.session_id,
-                   'trans-id': self.trans_id}
+                   'cmd-line': line,
+                   'body': cell}
         self.sock.send_string(json.dumps(request))
         print(json.loads(self.sock.recv()))   # Block!
-        self.sock.disconnect(self.endpoint)
 
-    @line_magic
-    def postprocess_mzn(self, line):
-        ctx = zmq.Context()
-        self.sock = ctx.socket(zmq.REQ)
-        self.sock.connect(self.endpoint)
-        request = {'action': 'post-process', 
-                   'session-id': self.session_id,
-                   'trans-id': self.trans_id}
-        self.sock.send_string(json.dumps(request))
-
-    @line_magic
-    def cleanup_mzn(self, line):
-        print(json.loads(self.sock.recv())) 
-        self.sock.disconnect(self.endpoint)
 
 
